4642ccb342f028e7a0c15898b26314804dc45da1
shikubro90 <shikubro90@gmail.com>
2023-03-17 04:44:59 +0600

    update

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,14 +1,19 @@
+import { Checkbox, Radio } from "antd";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 import ProductsCard from "../components/cards/ProductsCard";
+import { prices } from "../prices";
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    const [category, setCategory] = useState();
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
+  console.log(radio);
 
   useEffect(() => {
-    loadProducts();
+    if (!checked.length) loadProducts();
   }, []);
 
   const loadProducts = async () => {
@@ -20,6 +25,45 @@ const Shop = () => {
     } catch (error) {}
   };
 
+  const loadCategory = async () => {
+    try {
+      const { data } = await axios.get("/list");
+      setCategory(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loadCategory();
+  }, []);
+
+  const onHandleCheck = (value, id) => {
+    console.log(value, id);
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+      console.log(all);
+    } else {
+      all = all.filter((e) => e !== id);
+    }
+    setChecked(all);
+  };
+
+  const loadFiltered = async () => {
+    try {
+      const { data } = await axios.post("/filter-product", {
+        checked,
+        radio,
+      });
+      setProducts(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    if (checked.length || radio.length) loadFiltered();
+  }, [checked, radio]);
+
   return (
     <>
       <Jumbotron />
@@ -29,11 +73,34 @@ const Shop = () => {
             <h2 className="p-3 mt-2 mb-2 h4 bg-light text-center">
               Filter by Categories
             </h2>
+            <div className="row p-5">
+              {category?.map((category) => (
+                <Checkbox
+                  key={category._id}
+                  onChange={(e) =>
+                    onHandleCheck(e.target.checked, category._id)
+                  }
+                >
+                  {category.name}
+                </Checkbox>
+              ))}
+            </div>
             <div className="row">Check Box</div>
             <h2 className="p-3 mt-2 mb-2 h4 bg-light text-center">
               Filter by Price
             </h2>
-            <div className="row">Radio</div>
+            <div className="row p-5">
+              <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+                {prices.map((e) => (
+                  <div className="" key={e._id}>
+                    <Radio value={e.array}>{e.name}</Radio>
+                  </div>
+                ))}
+              </Radio.Group>
+            </div>
+            <div className="p-5 pt-0">
+              <button onClick={(e)=>window.location.reload()} className="btn btn-outline-secondary col-12">Reset</button>
+            </div>
           </div>
           <div className="col-md-9">
             <h2 className="p-3 mt-2 mb-2 h4 bg-light text-center">
