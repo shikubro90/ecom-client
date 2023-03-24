56a5f7c78d2de54ebcd6e8b9e514a66c834b2535
shikubro90 <shikubro90@gmail.com>
2023-03-25 03:53:12 +0600

    update

--- src/pages/ProductsCardView.jsx
+++ src/pages/ProductsCardView.jsx
@@ -6,37 +6,51 @@ import axios from "axios";
 const ProductsCardView = () => {
   const { slug } = useParams();
   const [product, setProduct] = useState({});
-  console.log(product);
-
+  const [related, setRelated] = useState([]);
   useEffect(() => {
-    loadProduct();
+    if (slug) loadProduct();
   }, [slug]);
+
+  
 
   const loadProduct = async () => {
     try {
       const { data } = await axios.get(`/products/${slug}`);
       setProduct(data);
+      loadRelated(data._id, data.category._id)
     } catch (error) {
       console.log(error);
     }
-    };
-    
-    // info obj 
-    const bootstrapInfo = {
-        buttonInfo: "!viewProduct",
-        titleInfo : 'fz-h1-fw-bold'
+  };
+
+  const loadRelated = async (productId,categoryId) => {
+    try {
+      const { data } = await axios.get(`/related-products/${productId}/${categoryId}`)
+      setRelated(data)
+    } catch (error) {
+      console.log(error)
     }
+  }
+
+
+  // info obj
+  const bootstrapInfo = {
+    buttonInfo: "!viewProduct",
+    titleInfo: "fz-h1-fw-bold",
+  };
 
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-9">
-          <ProductsCard p={product} bootstrapInfo={bootstrapInfo}  />
+          <ProductsCard p={product} bootstrapInfo={bootstrapInfo} />
         </div>
         <div className="col-md-3">
           <h2>Related Products</h2>
           <hr />
-          
+          {related?.map((e) => (
+            <ProductsCard key={e._id} p={e}/>
+          ))}
         </div>
       </div>
     </div>
