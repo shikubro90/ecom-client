2931dd79df6eaac10fb72f8b95d7e3d4aeee7a26
shikubro90 <shikubro90@gmail.com>
2023-04-11 05:32:28 +0600

    update

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,43 @@
-import React, { useState } from 'react'
-import Jumbotron from '../components/cards/Jumbotron'
-import { useCart } from '../context/cart'
-import { useAuth } from '../context/auth'
+import React, { useState } from "react";
+import Jumbotron from "../components/cards/Jumbotron";
+import { useCart } from "../context/cart";
+import { useAuth } from "../context/auth";
+import { useNavigate } from "react-router-dom";
+
 
 const Cart = () => {
-
-    const [cart, setCart] = useCart()
-    const [auth, setAuth] = useAuth()
-
+  const [cart, setCart] = useCart();
+  const [auth, setAuth] = useAuth();
+  const navigate = useNavigate()
   return (
-      <div>
-        <Jumbotron title={`Hello ${auth?.token && auth?.user?.name}`} subTitle={cart?.length ? `You have ${cart.length} items in cart. ${auth?.token? "" : "Please login to checkout"}` : `Cart is empty` } />
-    </div>
-  )
-}
+    <div>
+      <Jumbotron
+        title={`Hello ${auth?.token && auth?.user?.name}`}
+        subTitle={
+          cart?.length
+            ? `You have ${cart.length} items in cart. ${
+                auth?.token ? "" : "Please login to checkout"
+              }`
+            : `Cart is empty`
+        }
+      />
 
-export default Cart
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="p-3 mt-2 mb-2 h4 bg-light text-center">
+              {cart?.length ? (
+                "My Cart"
+              ) : (<div>
+                  <button className="btn btn-primary" onClick={()=>navigate("/")} >Continue Shopping</button>
+              </div>)}
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Cart;
